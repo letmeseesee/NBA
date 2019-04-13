ae2d90ef3d3e53cc2a10a9f3ec2e6a1469d114ea
wangyinghao <1903417374@qq.com>
2019-04-13 21:49:12 +0800

    登陆

--- src/main/java/com/nba/facade/vo/Course.java
+++
@@ -1,9 +0,0 @@
-package com.nba.facade.vo;
-
-import lombok.Data;
-
-@Data
-public class Course {
-    private String courseName;
-    private Integer code;
-}
--- src/main/java/com/nba/facade/vo/Student.java
+++
@@ -1,9 +0,0 @@
-package com.nba.facade.vo;
-
-import lombok.Data;
-
-@Data
-public class Student {
-    private String studentName;
-    private Integer studentAge;
-}
